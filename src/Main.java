3ffe0840771079e2fbc289ba6a10dd8bfd582eef
Berthold <berthold.fritz@hotmail.com>
2023-08-27 10:38:10 +0200

    Diassembler works.....

--- src/Main.java
+++ src/Main.java
@@ -8,24 +8,19 @@ public class Main {
 		byte [] ram=new byte[64000];
 		
 		ram[0x0600]=(byte) 169;
-		ram[0x601]=(byte) 255;
+		ram[0x601]=(byte) 30;
 		ram[0x602]=(byte) 0x48;
-		ram[0x603]=(byte) 0xa2;
-		ram[0x604]=(byte) 34;
-		ram[0x605]=(byte) 0xa0;
-		ram[0x606]=(byte) 11;
-		ram [0x607]=(byte)0x38;
-		ram [0x610]=(byte)0;
-		
-				
-				
-		myCPU.execute(ram, 0x600,true,20);
-		
+		ram[0x0603]=(byte) 169;
+		ram[0x604]=(byte) 0;
+		ram[0x605]=(byte) 0x68;
+	
+		myCPU.execute(ram,0x600);
 		System.out.println(myCPU.getP().printStatus());
 		System.out.println(myCPU.dumpStack(ram));
 		
-		myCPU.execute(ram, 0x600,false,20);
-	
+		
+		System.out.println(myCPU.dissasemble(ram, 0x0600, 0x0610));
+		
 
 	}
 
--- src/cpu/Cpu_6502.java
+++ src/cpu/Cpu_6502.java
@@ -8,24 +8,38 @@ package cpu;
  */
 public class Cpu_6502 {
 
+	//
+	// Specifies wether to execute the given code or to build a
+	// source code listing.
+	//
+	private static final boolean EXECUTE = true;
+	private static final boolean DIASSEMBLE = false;
+
+	//
+	// 6502 address space
+	//
 	private static final int START_ADDRESS_OF_STACK = 0x01ff;
 	private static final int END_ADDRESS_OF_STACK = 0x0100;
 
 	//
-	// Disassembled code
+	// RAM
 	//
-	StringBuilder diassembled = new StringBuilder();
+	private byte[] ram;
+
+	//
+	// Source
+	//
+	StringBuilder diassembled;
 
 	//
 	// Registers
 	//
-
-	int a, x, y; // Only 8- Bit's are used. They are infact 8 But registers
+	int a, x, y; // Only 8- Bit's are used. They are infact 8 Bit registers
 	int pc; // Programm counter always points to the next command to be executed
 	int s = -1; // Stack pointer (Stack is between $0100 and $01ff which is the top of stack)
 
 	//
-	// The Status register
+	// The 6502 status register
 	//
 	public class ProcessorStatus {
 		byte N; // Negative flag
@@ -37,31 +51,330 @@ public class Cpu_6502 {
 		byte Z; // Zero flag
 		byte C; // Carry flag
 
-		//
-		// A human readable form of the processor status register
-		//
+		/**
+		 * Show the status register.
+		 *
+		 * @return A string containing a human readable form of the register contents.
+		 */
 		public String printStatus() {
-
 			return (String.format("PC=$%04x", pc) + String.format(" SP=$%03x", START_ADDRESS_OF_STACK - s) + "// N=" + N
 					+ " V=" + V + " -=" + U + " B=" + B + " D=" + D + " I=" + I + " Z=" + Z + " C=" + C + " // A=" + a
 					+ " X=" + x + " Y=" + y + String.format(" // Status=$%02x", getStatusRegister()));
 		}
 
-		//
-		// Returns the status register as an 8 bit byte value
-		//
+		/**
+		 * A 8 bit value of the status register.
+		 * 
+		 * @return Status register.
+		 */
 		public byte getStatusRegister() {
 			byte s = (byte) (N * 1 + V * 2 + U * 4 + B * 8 + D * 16 + I * 32 + Z * 64 + C * 128);
 			return s;
 		}
 	}
+	public ProcessorStatus P;
 
-	public ProcessorStatus P = new ProcessorStatus();
+	/**
+	 * A new 6502- Processor.
+	 * 
+	 */
+	public Cpu_6502() {
+		this.P = new ProcessorStatus();
+	}
+
+	/**
+	 * Executes a series of commands, stored from start address upwards in specified
+	 * ram.
+	 * 
+	 * @param ram     Asscoiated ram.
+	 * @param address The address from where to run....
+	 * @return The resulting ram contents.
+	 */
+	public byte[] execute(byte[] ram, int address) {
+
+		this.ram = ram;
+
+		this.pc = address;
+
+		//
+		// Parse
+		//
+		while (P.B != 1) {
+			int command = unsignedByte(this.ram[pc]);
+			parser(command, EXECUTE);
+		}
+		return this.ram;
+	}
+
+	/**
+	 * Diassembles the ram contents.
+	 * 
+	 * @param ram Specifed ram.
+	 * @param start Start address.
+	 * @param end End address.
+	 * @return The diassemble source code listing.
+	 */
+	public String dissasemble(byte[] ram, int start, int end) {
+		this.ram = ram;
+		this.pc = start; // Set start, is updated py parser...
+		this.diassembled = new StringBuilder();
+
+		while (this.pc != end) {
+			int command = unsignedByte(this.ram[this.pc]);
+			parser(command, DIASSEMBLE);
+		}
+		return diassembled.toString();
+	}
+
+	/**
+	 * 6502
+	 * 
+	 * Either executes an command or returns the associated mnomic.
+	 * 
+	 * @param command The binary code of the 6502 command 
+	 * @param doExecute Execute=true, Diassemble=false....
+	 */
+	private void parser(int command, boolean doExecute) {
+		
+		switch (command) {
+
+		// brk
+		// I Flag
+		// push return address +2
+		// push status flag
+		//
+		case 0x00:
+			if (doExecute) {
+				P.B = 1;
+				s++;
+				this.ram[START_ADDRESS_OF_STACK - s] = (byte) 0xff; // Low byte of retuirn address ??
+				s++;
+				this.ram[START_ADDRESS_OF_STACK - s] = (byte) 0xff; // High byte of return address ??
+				s++;
+				this.ram[START_ADDRESS_OF_STACK - s] = (byte) (P.getStatusRegister());
+				// this.pc++;
+			} else {
+				diassembled.append(String.format("$%04x", this.pc)
+						+ String.format(" $%02x", unsignedByte(this.ram[this.pc])) + " brk\n");
+				this.pc++;
+			}
+			break;
+
+		// clc
+		// Carry flag
+		case 0x18:
+			if (doExecute) {
+				P.C = 0;
+				this.pc++;
+			} else {
+				diassembled.append(String.format("$%04x", this.pc)
+						+ String.format(" $%02x", unsignedByte(this.ram[this.pc])) + " clc\n");
+				this.pc++;
+			}
+			break;
+
+		// sec
+		// Carry flag
+		case 0x38:
+			if (doExecute) {
+				P.C = 1;
+				this.pc++;
+			} else {
+				diassembled.append(String.format("$%04x", this.pc)
+						+ String.format(" $%02x", unsignedByte(this.ram[this.pc])) + " sec\n");
+				this.pc++;
+			}
+			break;
+
+		// cli
+		// Interrupt flag
+		case 0x58:
+			if (doExecute) {
+				P.I = 0;
+				this.pc++;
+			} else {
+				diassembled.append(String.format("$%04x", this.pc)
+						+ String.format(" $%02x", unsignedByte(this.ram[this.pc])) + " cli\n");
+				this.pc++;
+			}
+			break;
+
+		// sei
+		// Interrupt flag
+		case 0x78:
+			if (doExecute) {
+				P.I = 1;
+				this.pc++;
+			} else {
+				diassembled.append(String.format("$%04x", this.pc)
+						+ String.format(" $%02x", unsignedByte(this.ram[this.pc])) + " sei\n");
+				this.pc++;
+			}
+			break;
+
+		// clv
+		// Overflow flag
+		case 0xb8:
+			if (doExecute) {
+				P.V = 0;
+				this.pc++;
+			} else {
+				diassembled.append(String.format("$%04x", this.pc)
+						+ String.format(" $%02x", unsignedByte(this.ram[this.pc])) + " clv\n");
+				this.pc++;
+			}
+			break;
+
+		// cld
+		// Decimal flag
+		case 0xd8:
+			if (doExecute) {
+				P.D = 0;
+				this.pc++;
+			} else {
+				diassembled.append(String.format("$%04x", this.pc)
+						+ String.format(" $%02x", unsignedByte(this.ram[this.pc])) + " cld\n");
+				this.pc++;
+			}
+			break;
+
+		// sed
+		// decimal flag
+		case 0xf8:
+			if (doExecute) {
+				P.D = 1;
+				this.pc++;
+			} else {
+				diassembled.append(String.format("$%04x", this.pc) + String.format(" $%02x", unsignedByte(this.ram[pc]))
+						+ " sed\n");
+				this.pc++;
+			}
+			break;
+
+		// pha
+		//
+		case 0x48:
+			if (doExecute) {
+				s++;
+				this.ram[START_ADDRESS_OF_STACK - s] = (byte) a;
+				this.pc++;
+			} else {
+				diassembled.append(String.format("$%04x", this.pc)
+						+ String.format(" $%02x", unsignedByte(this.ram[this.pc])) + " pha\n");
+				this.pc++;
+			}
+			break;
+
+		// pla
+		// A,Z and N.
+		case 0x68:
+			if (doExecute) {
+				a = unsignedByte(this.ram[(START_ADDRESS_OF_STACK-s)]);
+
+				if (a == 0)
+					P.Z = 1;
+				else
+					P.Z = 0;
+
+				if (a > 127)
+					P.N = 1;
+				else
+					P.N = 0;
+				s--;
+				this.pc++;
+			} else {
+				diassembled.append(String.format("$%04x", this.pc) + String.format(" $%02x", unsignedByte(ram[this.pc]))
+						+ " pla\n");
+				this.pc++;
+			}
+			break;
+
+		// ldy #b
+		// Z,N Flags
+		case 0xa0:
+			if (doExecute) {
+				this.pc++;
+				y = unsignedByte(this.ram[this.pc]);
+
+				if (y == 0)
+					P.Z = 1;
+				else
+					P.Z = 0;
+
+				if (y > 127)
+					P.N = 1;
+				else
+					P.N = 0;
+				this.pc++;
+
+			} else {
+				diassembled.append(String.format("$%04x", this.pc)
+						+ String.format(" $%02x", unsignedByte(this.ram[this.pc])) + " ldy #");
+				this.pc++;
+				diassembled.append(String.format("$%02x", this.ram[this.pc]) + "\n");
+				this.pc++;
+			}
+			break;
+
+		// ldx #b
+		// Z,N Flags
+		case 0xa2:
+			if (doExecute) {
+				this.pc++;
+				x = unsignedByte(this.ram[this.pc]);
+
+				if (x == 0)
+					P.Z = 1;
+				else
+					P.Z = 0;
+
+				if (x > 127)
+					P.N = 1;
+				else
+					P.N = 0;
+				this.pc++;
+			} else {
+				diassembled.append(String.format("$%04x", this.pc)
+						+ String.format(" $%02x", unsignedByte(this.ram[this.pc])) + " ldx #");
+				this.pc++;
+				diassembled.append(String.format("$%02x", this.ram[this.pc]) + "\n");
+				this.pc++;
+			}
+			break;
+
+		// lda #b
+		// Z,N Flags
+		case 169:
+			if (doExecute) {
+				this.pc++;
+				a = unsignedByte(this.ram[pc]);
+
+				if (a == 0)
+					P.Z = 1;
+				else
+					P.Z = 0;
+
+				if (a > 127)
+					P.N = 1;
+				else
+					P.N = 0;
+				this.pc++;
+
+			} else {
+				diassembled.append(
+						String.format("$%04x", this.pc) + String.format(" $%02x", unsignedByte(this.ram[this.pc])) + " lda #");
+				this.pc++;
+				diassembled.append(String.format("$%02x", this.ram[this.pc]) + "\n");
+				this.pc++;
+			}
+			break;
+		}
+	}
 
 	/**
 	 * Shows the contents of the current stack.
 	 * 
-	 * @param ram Ram of the host machine.
+	 * @pram ram Ram of the host machine.
 	 * @return String containing the contents of the stack.
 	 */
 	public String dumpStack(byte[] ram) {
@@ -75,284 +388,10 @@ public class Cpu_6502 {
 	}
 
 	/**
-	 * The diassembler. If the cpu medel was run with the 'execute' option set to
-	 * false, a diassembled source code listing is returned.
+	 * Other getters....
 	 * 
-	 * @return The diasembled code.
+	 * @return
 	 */
-	public String getDiassembledCode() {
-		return diassembled.toString();
-	}
-
-	/**
-	 * The Cpu- Model
-	 * 
-	 */
-	public Cpu_6502() {
-
-	}
-
-	/**
-	 * Executes or diassembles a series of commands, stored from start address
-	 * upwards in specified ram.
-	 * 
-	 * @param ram     Asscoiated ram.
-	 * @param address The address from where to run....
-	 * @return The resulting ram contents.
-	 */
-	public byte[] execute(byte[] ram, int address, boolean doExecute, int length) {
-
-		pc = address;
-
-		//
-		// Parser
-		//
-		while (P.B != 1 && length > 0) {
-
-			int command = unsignedByte(ram[pc]);
-
-			switch (command) {
-
-			// brk
-			// I Flag
-			// push return address +2
-			// push status flag
-			//
-			case 0x00:
-				if (doExecute) {
-					P.B = 1;
-					s++;
-					ram[START_ADDRESS_OF_STACK - s] = (byte) (pc + 2 / 256); // Low byte of retuirn address ??
-					s++;
-					ram[START_ADDRESS_OF_STACK - s] = (byte) (pc + 2); // High byte of return address ??
-					s++;
-					ram[START_ADDRESS_OF_STACK - s] = (byte) (P.getStatusRegister());
-					// pc++;
-				} else {
-					diassembled.append(
-							String.format("$%04x", pc) + String.format(" $%02x", unsignedByte(ram[pc])) + " brk\n");
-					pc++;
-				}
-				break;
-
-			// clc
-			// Carry flag
-			case 0x18:
-				if (doExecute) {
-					P.C = 0;
-					pc++;
-				} else {
-					diassembled.append(
-							String.format("$%04x", pc) + String.format(" $%02x", unsignedByte(ram[pc])) + " clc\n");
-					pc++;
-				}
-				break;
-
-			// sec
-			// Carry flag
-			case 0x38:
-				if (doExecute) {
-					P.C = 1;
-					pc++;
-				} else {
-					diassembled.append(
-							String.format("$%04x", pc) + String.format(" $%02x", unsignedByte(ram[pc])) + " sec\n");
-					pc++;
-				}
-				break;
-
-			// cli
-			// Interrupt flag
-			case 0x58:
-				if (doExecute) {
-					P.I = 0;
-					pc++;
-				} else {
-					diassembled.append(
-							String.format("$%04x", pc) + String.format(" $%02x", unsignedByte(ram[pc])) + " cli\n");
-					pc++;
-				}
-				break;
-
-			// sei
-			// Interrupt flag
-			case 0x78:
-				if (doExecute) {
-					P.I = 1;
-					pc++;
-				} else {
-					diassembled.append(
-							String.format("$%04x", pc) + String.format(" $%02x", unsignedByte(ram[pc])) + " sei\n");
-					pc++;
-				}
-				break;
-
-			// clv
-			// Overflow flag
-			case 0xb8:
-				if (doExecute) {
-					P.V = 0;
-					pc++;
-				} else {
-					diassembled.append(
-							String.format("$%04x", pc) + String.format(" $%02x", unsignedByte(ram[pc])) + " clv\n");
-					pc++;
-				}
-				break;
-
-			// cld
-			// Decimal flag
-			case 0xd8:
-				if (doExecute) {
-					P.D = 0;
-					pc++;
-				} else {
-					diassembled.append(
-							String.format("$%04x", pc) + String.format(" $%02x", unsignedByte(ram[pc])) + " cld\n");
-					pc++;
-				}
-				break;
-
-			// sed
-			// decimal flag
-			case 0xf8:
-				if (doExecute) {
-					P.D = 1;
-					pc++;
-				} else {
-					diassembled.append(
-							String.format("$%04x", pc) + String.format(" $%02x", unsignedByte(ram[pc])) + " sed\n");
-					pc++;
-				}
-				break;
-
-			// pha
-			//
-			case 0x48:
-				if (doExecute) {
-					s++;
-					ram[START_ADDRESS_OF_STACK - s] = (byte) a;
-					pc++;
-				} else {
-					diassembled.append(
-							String.format("$%04x", pc) + String.format(" $%02x", unsignedByte(ram[pc])) + " pha\n");
-					pc++;
-				}
-				break;
-
-			// pla
-			// A,Z and N.
-			case 0x68:
-				if (doExecute) {
-					a = unsignedByte(ram[START_ADDRESS_OF_STACK - s]);
-					s--;
-					if (a == 0)
-						P.Z = 1;
-					else
-						P.Z = 0;
-
-					if (a > 127)
-						P.N = 1;
-					else
-						P.N = 0;
-
-					pc++;
-				} else {
-					diassembled.append(
-							String.format("$%04x", pc) + String.format(" $%02x", unsignedByte(ram[pc])) + " pla\n");
-					pc++;
-				}
-				break;
-
-			// ldy #b
-			// Z,N Flags
-			case 0xa0:
-				if (doExecute) {
-					pc++;
-					y = unsignedByte(ram[pc]);
-
-					if (y == 0)
-						P.Z = 1;
-					else
-						P.Z = 0;
-
-					if (y > 127)
-						P.N = 1;
-					else
-						P.N = 0;
-					pc++;
-
-				} else {
-					diassembled.append(
-							String.format("$%04x", pc) + String.format(" $%02x", unsignedByte(ram[pc])) + " ldy #");
-					pc++;
-					diassembled.append(String.format("$%02x", ram[pc]) + "\n");
-					pc++;
-				}
-				break;
-
-			// ldx #b
-			// Z,N Flags
-			case 0xa2:
-				if (doExecute) {
-					pc++;
-					x = unsignedByte(ram[pc]);
-
-					if (x == 0)
-						P.Z = 1;
-					else
-						P.Z = 0;
-
-					if (x > 127)
-						P.N = 1;
-					else
-						P.N = 0;
-					pc++;
-				} else {
-					diassembled.append(
-							String.format("$%04x", pc) + String.format(" $%02x", unsignedByte(ram[pc])) + " ldx #");
-					pc++;
-					diassembled.append(String.format("$%02x", ram[pc]) + "\n");
-					pc++;
-				}
-				break;
-
-			// lda #b
-			// Z,N Flags
-			case 169:
-				if (doExecute) {
-					pc++;
-					a = unsignedByte(ram[pc]);
-
-					if (a == 0)
-						P.Z = 1;
-					else
-						P.Z = 0;
-
-					if (a > 127)
-						P.N = 1;
-					else
-						P.N = 0;
-					pc++;
-
-				} else {
-					diassembled.append(
-							String.format("$%04x", pc) + String.format(" $%02x", unsignedByte(ram[pc])) + " lda #");
-					pc++;
-					diassembled.append(String.format("$%02x", ram[pc]) + "\n");
-					pc++;
-				}
-				break;
-			}
-
-			//
-			// For the diassembler solely...
-			//
-			length--;
-		}
-		System.out.println(diassembled.toString());
-		return ram;
-	}
 
 	public ProcessorStatus getP() {
 		return P;
